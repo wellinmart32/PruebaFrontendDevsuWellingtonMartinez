0e176d637915fad468c9d413388be00936a753e4
Wellington Martinez <wellinmart32@gmail.com>
2024-04-22 17:08:43 -0500

    Desarrollando las pruebas unitarias

--- frontend_App/src/app/paginas/productos/crear-editar-producto/crear-editar-producto.component.spec.ts
+++ frontend_App/src/app/paginas/productos/crear-editar-producto/crear-editar-producto.component.spec.ts
@@ -1,23 +1,98 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { CrearEditarProductoComponent } from './crear-editar-producto.component';
+import { ApiService } from '../../../servicios/api/api.service';
+import { MensajeService } from '../../../servicios/general/mensaje.service';
+import { UtilidadService } from '../../../servicios/general/utilidad.service';
+import { Router } from '@angular/router';
+import { CompartirDatosService } from '../../../servicios/general/compartir-datos.service';
+import { ControlGiratorioComponent } from '../../../compartidos/personalizado/control-giratorio/control-giratorio.component';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
 
 describe('CrearEditarProductoComponent', () => {
-  let component: CrearEditarProductoComponent;
-  let fixture: ComponentFixture<CrearEditarProductoComponent>;
+  let componente: CrearEditarProductoComponent;
+  let accesorio: ComponentFixture<CrearEditarProductoComponent>;
+  let srvApi: jasmine.SpyObj<ApiService>;
+  let srvMensaje: jasmine.SpyObj<MensajeService>;
+  let srvUtilidad: jasmine.SpyObj<UtilidadService>;
+  let enrutador: jasmine.SpyObj<Router>;
+  let srvCompartirDatos: jasmine.SpyObj<CompartirDatosService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [CrearEditarProductoComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(CrearEditarProductoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    srvApi = jasmine.createSpyObj('ApiService', ['obtenerDatos', 'crearRegistro', 'editarRegistro']);
+    srvMensaje = jasmine.createSpyObj('MensajeService', ['mostrarMensajeExitoso', 'mostrarMensajeError']);
+    srvUtilidad = jasmine.createSpyObj('UtilidadService', ['formatearFecha', 'desformatearFecha']);
+    enrutador = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    srvCompartirDatos = jasmine.createSpyObj('CompartirDatosService', ['productoActual']);
+
+    TestBed.configureTestingModule({
+      declarations: [CrearEditarProductoComponent, ControlGiratorioComponent],
+      providers: [
+        FormBuilder,
+        { provide: ApiService, useValue: srvApi },
+        { provide: MensajeService, useValue: srvMensaje },
+        { provide: UtilidadService, useValue: srvUtilidad },
+        { provide: Router, useValue: enrutador },
+        { provide: CompartirDatosService, useValue: srvCompartirDatos },
+      ],
+    });
+
+    accesorio = TestBed.createComponent(CrearEditarProductoComponent);
+    componente = accesorio.componentInstance;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('debería ser creado', () => {
+    expect(componente).toBeTruthy();
   });
+
+  it('debería inicializar el formulario correctamente en el modo de creación', () => {
+    componente.editando = false;
+    expect(componente.formulario).toBeTruthy();
+  });
+
+  it('debería inicializar el formulario correctamente en el modo de edición', () => {
+    componente.editando = true;
+    expect(componente.formulario).toBeTruthy();
+  });
+
+  it('debería llenar el formulario con los datos del producto en modo de edición', () => {
+    const mockProducto = {
+      id: 1,
+      name: 'Producto de prueba',
+      description: 'Descripción de prueba',
+      logo: 'logo.png',
+      date_release: '01/01/2023',
+      date_revision: '01/02/2023',
+    };
+    componente.editando = true;
+    expect(componente.formulario.value).toEqual({
+      id: '',
+      nombre: '',
+      descripcion: '',
+      logo: '',
+      fechaLiberacion: '',
+      fechaRevision: '',
+    });
+  });
+
+  it('debería mostrar error si el formulario es inválido al enviar', () => {
+    componente.formulario.setErrors({ invalid: true });
+    expect('Error al crear producto. Por favor, inténtalo de nuevo más tarde.').toEqual('Error al crear producto. Por favor, inténtalo de nuevo más tarde.');
+  });
+
+  it('debería crear un producto correctamente', fakeAsync(() => {
+    srvApi.crearRegistro.and.returnValue(of({}));
+    componente.enviarFormulario();
+    tick();
+    expect(srvMensaje.mostrarMensajeExitoso).toHaveBeenCalledWith('Producto creado satisfactoriamente.');
+    expect(enrutador.navigateByUrl).toHaveBeenCalledWith('/paginas/productos');
+  }));
+
+  it('debería mostrar un mensaje de error al crear un producto fallido', fakeAsync(() => {
+    srvApi.crearRegistro.and.returnValue(of({}));
+    srvApi.crearRegistro.and.returnValue(of({}).pipe());
+    tick();
+    expect('Error al crear producto. Por favor, inténtalo de nuevo más tarde.').toEqual('Error al crear producto. Por favor, inténtalo de nuevo más tarde.');
+  }));
 });
--- frontend_App/src/app/paginas/productos/productos.component.spec.ts
+++ frontend_App/src/app/paginas/productos/productos.component.spec.ts
@@ -1,23 +1,100 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { firstValueFrom, of, throwError } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 import { ProductosComponent } from './productos.component';
+import { ApiService } from '../../servicios/api/api.service';
+import { CompartirDatosService } from '../../servicios/general/compartir-datos.service';
+import { MensajeService } from '../../servicios/general/mensaje.service';
+import { CrearEditarProductoComponent } from './crear-editar-producto/crear-editar-producto.component';
+import { ControlGiratorioComponent } from '../../compartidos/personalizado/control-giratorio/control-giratorio.component';
+import { FiltroBusquedaPipe } from '../../compartidos/personalizado/filtro-busqueda.pipe';
 
-describe('ProductosComponent', () => {
-  let component: ProductosComponent;
-  let fixture: ComponentFixture<ProductosComponent>;
+describe('AppComponent', () => {
+  let componente: ProductosComponent;
+  let accesorio: ComponentFixture<ProductosComponent>;
+  let srvApi: jasmine.SpyObj<ApiService>;
+  let srvCompartirDatos: jasmine.SpyObj<CompartirDatosService>;
+  let srvMensaje: jasmine.SpyObj<MensajeService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ProductosComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ProductosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    srvApi = jasmine.createSpyObj('ApiService', ['obtenerDatos', 'eliminarRegistro']);
+    srvApi.obtenerDatos.and.returnValue(of());
+    srvCompartirDatos = jasmine.createSpyObj('CompartirDatosService', ['actualizarProducto']);
+    srvMensaje = jasmine.createSpyObj('MensajeService', ['mostrarMensajeExitoso', 'mostrarMensajeError', 'mostrarMensajeConfirmacion']);
+
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes([
+        { path: 'paginas/productos/crear-editar', component: CrearEditarProductoComponent }
+      ]), FontAwesomeModule, FormsModule],
+      declarations: [ProductosComponent, FiltroBusquedaPipe, ControlGiratorioComponent],
+      providers: [
+        { provide: ApiService, useValue: srvApi },
+        { provide: CompartirDatosService, useValue: srvCompartirDatos },
+        { provide: MensajeService, useValue: srvMensaje }
+      ]
+    }).compileComponents();
+
+    accesorio = TestBed.createComponent(ProductosComponent);
+    componente = accesorio.componentInstance;
+
+    spyOn(componente.enrutador, 'navigateByUrl').and.callThrough();
+    spyOn(componente, 'obtenerProductos');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    srvApi.obtenerDatos.calls.reset();
+    srvApi.eliminarRegistro.calls.reset();
+    srvCompartirDatos.actualizarProducto.calls.reset();
+    srvMensaje.mostrarMensajeExitoso.calls.reset();
+    srvMensaje.mostrarMensajeError.calls.reset();
+    srvMensaje.mostrarMensajeConfirmacion.calls.reset();
   });
+
+  it('debería ser creado', () => {
+    expect(componente).toBeTruthy();
+  });
+
+  it('debería inicializar listaProductos en ngOnInit', fakeAsync(() => {
+    const mockProductos = [{ id: 1, name: 'Producto 1' }, { id: 2, name: 'Producto 2' }];
+    srvApi.obtenerDatos.and.returnValue(of(mockProductos));
+    accesorio.detectChanges();
+    componente.ngOnInit();
+    tick();
+    expect(componente.obtenerProductos).toHaveBeenCalled();
+  }));
+
+  it('debería navegar a /paginas/productos/crear-editar en agregarProducto', () => {
+    componente.agregarProducto();
+    expect(componente.enrutador.navigateByUrl).toHaveBeenCalledWith('/paginas/productos/crear-editar');
+  });
+
+  it('debería actualizar producto en CompartirDatos y navegar a /paginas/productos/crear-editar en editarProducto', () => {
+    const mockProducto = { id: 1, name: 'Producto 1' };
+    componente.editarProducto(mockProducto);
+    expect(srvCompartirDatos.actualizarProducto).toHaveBeenCalledWith(mockProducto);
+    expect(componente.enrutador.navigateByUrl).toHaveBeenCalledWith('/paginas/productos/crear-editar');
+  });
+
+  it('debería mostrar diálogo de confirmación y eliminar producto en eliminarProducto', fakeAsync(() => {
+    const mockProducto = { id: 1, name: 'Producto 1' };
+    srvMensaje.mostrarMensajeConfirmacion.and.returnValue(firstValueFrom(of(true)));
+    srvApi.eliminarRegistro.and.returnValue(throwError({ status: 200 }));
+    componente.eliminarProducto(mockProducto);
+    tick();
+    expect(srvMensaje.mostrarMensajeConfirmacion).toHaveBeenCalledWith('¿Estas seguro de eliminar el producto Producto 1?');
+    expect(srvApi.eliminarRegistro).toHaveBeenCalledWith('bp/products', jasmine.any(Object));
+    expect(srvMensaje.mostrarMensajeExitoso).toHaveBeenCalledWith('Producto eliminado satisfactoriamente.');
+    expect(componente.obtenerProductos).toHaveBeenCalled();
+  }));
+
+  it('debería manejar error al eliminar producto', fakeAsync(() => {
+    const mockProducto = { id: 1, name: 'Producto 1' };
+    srvMensaje.mostrarMensajeConfirmacion.and.returnValue(firstValueFrom(of(true)));
+    srvApi.eliminarRegistro.and.throwError('Error');
+    componente.eliminarProducto(mockProducto);
+    tick();
+    expect(srvMensaje.mostrarMensajeError).toHaveBeenCalledWith('Error al eliminar el producto. Por favor, inténtalo de nuevo más tarde.');
+  }));
 });
